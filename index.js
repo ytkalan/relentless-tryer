28e08bce5688a6326527ed6e04a643ee2a418cae
ytkalan <atomyuen95@gmail.com>
2019-05-09 22:28:59 +0800

    complete logic

--- index.js
+++ index.js
@@ -6,31 +6,29 @@ const path = require('chromedriver').path;
 const notifier = require('node-notifier');
 
 const dotenv = require('dotenv').config();
-
-const counter = 1;
 const service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
 
 const driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-const reconnect = () => {
-    driver
-      .get(process.env.url)
-      .then(() => {
-          console.log("Attempt: ", counter);
-          driver
-          .findElement(By.xpath(process.env.img_xpath))
-          .then(() => {
-            notifier.notify('Success!');
-            driver
-            .findElement(By.xpath(process.env.button_xpath)).click()
-            .catch(() => console.log('End'));
-          })
-          .catch(() => {
-            counter = count + 1;
-            setTimeout(reconnect, 500);
-        });
-      });
-   };
+const customFunction = () => {
+  console.log('Do anything');
+};
+
+const reconnect = (count = 1) => {
+  driver
+    .get(process.env.url)
+    .then(async() => {
+      console.log(`Attempt: ${count}`);
+      await driver
+        .findElement(By.xpath(process.env.img_xpath))
+        .then(() => {
+          notifier.notify({ title: 'Connected!', message: `Successful after ${count} attempts`});
+          customFunction();
+          return Promise.resolve()
+        })
+        .catch(() => (setTimeout(() => reconnect(count + 1), 50)));
+    });
+};
    
-   reconnect();
+reconnect();
